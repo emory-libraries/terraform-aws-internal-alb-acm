c0c244a013144e2597100bb1d1d06dfe89869fd6
Solomon Hilliard <solomon.hilliard@emory.edu>
2021-01-12 13:57:02 -0500

    Add sensitive flag to cert vars

--- variables.tf
+++ variables.tf
@@ -1,16 +1,19 @@
 variable "private_key_file" {
   type = string
   description = "Path to a the PEM-formatted private key for the avalon website. This will be loaded into AWS Certificate Manager(ACM) and used by the Application Load Balancer(ALB). Max supported RSA Key size is 2048"
+  sensitive = true
 }
 
 variable "certificate_body_file" {
   type = string
   description = "Path the PEM-formatted signed certificate for the avalon website, this will be loaded into ACM and used by the ALB."
+  sensitive = true
 }
 
 variable "certificate_chain_file" {
   type = string
   description = "Path to the PEM-formatted interm or chain certificate, some regions may require the chain cert be in 'reverse' format."
+  sensitive = true
 }
 
 variable "stack_name" {
