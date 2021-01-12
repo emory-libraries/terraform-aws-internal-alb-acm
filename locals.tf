8f4316d65eaeb5b2a812f504e779eed1b374c337
Solomon Hilliard <solomon.hilliard@emory.edu>
2021-01-11 19:02:06 -0500

    Add application fdqn logic

--- locals.tf
+++ locals.tf
@@ -1,5 +1,8 @@
 locals {
     namespace = "${var.stack_name}-${terraform.workspace}"
+    application_fqdn_list = split(".", var.application_fqdn)
+    appended_fqdn_list = terraform.workspace == "prod" ? local.application_fqdn_list : formatlist("%s-${terraform.workspace}", local.application_fqdn_list)
+    appended_fqdn = replace(var.application_fqdn, local.application_fqdn_list[var.application_fqdn_workspace_insertion_index], "${local.appended_fqdn_list[var.application_fqdn_workspace_insertion_index]}")
     common_tags = merge(
     var.tags,
     {
--- variables.tf
+++ variables.tf
@@ -1,3 +1,20 @@
+variable "application_fqdn" {
+  type = string
+  description = <<EOF
+    The fully qualified production domain name. This is name is used only by the application load balancer, not route53.
+    Note that the template will also create another domain name for streaming that is streaming.{application_fqdn}.
+    EOF
+}
+variable "application_fqdn_workspace_insertion_index" {
+  type = number
+  default = 0
+  description = <<EOF
+    The application fqdn is split into a list at each '.', this variable is the index (first object is 0) where the workspace will be appended.
+    For example if the application fqdn is 'avr.emory.edu', this variable is set to 0, and the workspace is test, the output will be avr-test.emory.edu.
+    If the workspace is 'prod' then nothing is appended to the fqdn and the address on the alb would be 'avr.emory.edu'. 
+    EOF
+}
+
 variable "private_key_file" {
   type = string
   description = "Path to a the PEM-formatted private key for the avalon website. This will be loaded into AWS Certificate Manager(ACM) and used by the Application Load Balancer(ALB). Max supported RSA Key size is 2048"
