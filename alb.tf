9da975ef9d715e7370954b6154458232d741c52c
Solomon Hilliard <solomon.hilliard@emory.edu>
2021-01-12 13:52:39 -0500

    Remove target group logic and vars

--- alb.tf
+++ alb.tf
@@ -39,23 +39,6 @@ resource "aws_alb_listener" "alb_listener" {
   }
 }
 
-# Web listener rule and target group
-resource "aws_lb_listener_rule" "alb_web_listener_rule" {
-  listener_arn = aws_alb_listener.alb_listener.arn
-  priority     = 99
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_alb_target_group.target_group.arn
-  }
-
-  condition {
-    host_header {
-      values = [local.appended_fqdn]
-    }
-  }
-}
-
 # Import web and streaming certificate 
 resource "aws_acm_certificate" "alb_cert" {
   private_key = file(var.private_key_file)
@@ -66,26 +49,5 @@ resource "aws_acm_certificate" "alb_cert" {
 
   lifecycle {
     create_before_destroy = true
-  }
-}
-
-resource "aws_alb_target_group" "target_group" {
-  name     = "${local.namespace}-alb-web"
-  port     = "80"
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
-
-  stickiness {
-    type            = "lb_cookie"
-    cookie_duration = 1800
-    enabled         = "true"
-  }
-  health_check {
-    healthy_threshold   = 3
-    unhealthy_threshold = 10
-    timeout             = 5
-    interval            = 30
-    path                = "/"
-    port                = "80"
   }
 }
